22f200c974a7cccd2bdd8c240a878b46e05349b3
Salahudin Zumberi <zumbesa1@students.zhaw.ch>
2021-06-11 12:45:48 +0200

    fix Test CleanQualityTest

--- backend/src/test/java/ch/zhaw/iwi/devops/cleanquality/CleanQualityTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/cleanquality/CleanQualityTest.java
@@ -7,7 +7,10 @@ public class CleanQualityTest {
     public void cleanquality() {
 
         CleanQuality cq = new CleanQuality();
+        Assert.assertEquals("Not Clean", cq.getAlertCleanQuality(3));
+        Assert.assertEquals("Semi Clean", cq.getAlertCleanQuality(2));
         Assert.assertEquals("Clean", cq.getAlertCleanQuality(1));
+        Assert.assertEquals("The input has to be the number 1, 2 or 3.", cq.getAlertCleanQuality(4));       
 
     }
 }
